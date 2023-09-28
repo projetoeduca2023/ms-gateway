407d4544879704431eaa2caece79d33530b63976
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2023-09-28 18:13:55 -0300

    init

---
+++ src/test/java/br/com/educacao/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.educacao.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
